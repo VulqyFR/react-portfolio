2807957cc26ae162bd9b7319b3ace743b5a8b476
Vulqy <vulqy.pro@gmail.com>
2023-12-10 23:03:12 +0100

    feat: language button ui update

--- src/components/header/LanguageButton.jsx
+++ src/components/header/LanguageButton.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLanguageContext } from '../../hooks/useLanguageContext';
 import { Globe } from 'react-feather';
 
 const LanguageButton = ({ windowWidth, limitWidth }) => {
   const { language, changeLanguage } = useLanguageContext();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleLanguageChange = () => {
-    changeLanguage(language === 'en' ? 'fr' : 'en');
-  };
+  const handleClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
 
   return (
-    <button onClick={handleLanguageChange} className={` navbar__button ${windowWidth > limitWidth ? 'navbar__button--language' : 'navbar__button--language--screen'}`}>
+    <>
+    <button onClick={handleClick} className={` navbar__button ${windowWidth > limitWidth ? 'navbar__button--language' : 'navbar__button--language--screen'}`}>
       <Globe/>
+      {isMenuOpen && (
+      <div className='navbar__langmenu--wrapper'>
+          <ul className='navbar__langmenu-list'>
+            <a onClick={() => changeLanguage('en')} className='navbar__langmenu-list-link'><li>ENGLISH (EN)</li></a>
+            <a onClick={() => changeLanguage('fr')} className='navbar__langmenu-list-link'><li>FRANÇAIS (FR)</li></a>
+          </ul>
+      </div>
+    )}
     </button>
+    </>
   );
 };
 
