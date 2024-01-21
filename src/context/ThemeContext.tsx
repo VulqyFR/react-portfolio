2873c90e2fd80823851d05ad2d220ade8667e3e3
VulqyFR <vulqy.pro@gmail.com>
2024-01-21 23:57:04 +0100

    feat: theme changer

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,60 @@
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+// On défini ici le type ce qui sera stocké dans notre contexte.
+type ContextType = {
+  // Une fonction pour switch entre light et dark
+  toggleDark: () => void;
+  // La valeur effective du thème
+  isDark: boolean;
+};
+
+// Cette variable contient le contexte par défaut qui sera
+// (sauf en cas de bug) toujours écrasé.
+const defaultContext: ContextType = {
+  toggleDark: () => {
+    console.warn("Should have been overriden");
+  },
+  isDark: true,
+};
+
+// On crée le contexte à partir du contexte par défaut.
+const ThemeContext = createContext(defaultContext);
+
+// On défini et exporte un component pour
+// wrap le provider de React afin de définir
+// un contexte basé sur un useState
+export const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
+  const [isDark, setIsDark] = useState(true);
+
+  // Après que ce component ait chargé on va lire le local storage.
+  useEffect(() => {
+    const lsDark = JSON.parse(
+      localStorage.getItem("ThemeContext:isDark") as string
+    );
+    if (lsDark !== undefined && lsDark !== null) {
+      setIsDark(lsDark);
+      // detecte si l'utilisateur demande explicitement un thème clair.
+    } else if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: light)").matches
+    ) {
+      setIsDark(false);
+    }
+  }, []);
+
+  const context: ContextType = {
+    toggleDark: () => {
+      // A chaque fois qu'on toggle le thème on écrit dans le local storage
+      localStorage.setItem("ThemeContext:isDark", String(!isDark));
+      setIsDark(!isDark);
+    },
+    isDark,
+  };
+  // On génère un provider avec notre contexte
+  return (
+    <ThemeContext.Provider value={context}>{children}</ThemeContext.Provider>
+  );
+};
+
+// Par défaut le context est exporté et pas le provider.
+export default ThemeContext;
